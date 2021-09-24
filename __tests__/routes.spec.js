1a35e6ddb9be9b555c5ade40143543026261b31e
Zain Uppal <zainuppal1@gmail.com>
2021-09-24 15:57:26 +0100

    comit

--- __tests__/routes.spec.js
+++ __tests__/routes.spec.js
@@ -2,11 +2,46 @@ const request = require('supertest');
 const app = require('../server');
 
 describe('api routes', () => {
-    test('GET / returns a message', (done) => {
-        request(app)
-            .get('/')
+    let api;
+
+    beforeAll(() => {
+        api = app.listen(5000, () => {
+            console.log("testing")
+        })
+    })
+
+    afterAll((done) => {
+        console.log("api end")
+        api.close(done)
+    })
+
+    test('GET /cars returns array of cars', (done) => {
+        request(api)
+            .get('/cars')
             .set('Accept', 'application/json')
             .expect('Content-Type', /json/)
+            .expect(200,done)
+    })
+    test('GET /cars/:hid returns data about a specific cars', (done) => {
+        request(api)
+            .get('/cars/1')
+            .set('Accept', 'application/json')
+            .expect({ id:1, name: "mercedes", type: "amg"})
+            .expect('Content-Type', /json/)
             .expect(200, done)
+    })
+    test('POST /cars', (done) => {
+        request(api)
+            .post('/cars')
+            .send({name: "bmw", type: "random"})
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect({ message: `done adding`})
+            .expect(201, done)
+    })
+    test('GET /trash', (done) => {
+        request(api)
+            .get('/random')
+            .expect(404, done)
     })
 })
